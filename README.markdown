ace7e53a775cf640466a850155642779c514d8cb
prakhar <prakhar1989@gmail.com>
2012-01-31 15:11:49 +0530

    First Commit

---
+++ README.markdown
@@ -0,0 +1,10 @@
+Course Web Portal
+====================
+
+Course registration portal made as a part
+of ISG @ IIM Calcutta
+
+Technologies used:
+-------------------
+- Code Ignitor (PHP)
+- jQuery
---
+++ app/views/index_page.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Course Registration</title>
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="assets/css/custom.css" type="text/css" media="screen" charset="utf-8"/>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link rel="shortcut icon" href="assets/img/favicon.png">
+    <style type="text/css">
+      body {
+        padding-top: 40px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="topbar">
+      <div class="fill">
+        <div class="container">
+          <a class="brand" href="#top">Course Registration</a>
+          <ul class="nav pull-left">
+            <li><a href="#slot1">Slot 1</a></li>
+            <li><a href="#slot2">Slot 2</a></li>
+            <li><a href="#slot3">Slot 3</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="header" id="top">
+      <div class="container">
+        <h1>COURSE REGISTRATION PORTAL</h1>
+        <div class="iimc">
+          <h2>IIM Calcutta</h2>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <!--Table for Slot1 -->
+      <section id="slot1">
+        <div class="page-header">
+          <h3>Slot 1</h3>
+        </div>
+        <div class="row">
+          <div class="span12">
+            <table class="bordered-table zebra-striped">
+              <thead>
+                <tr>
+                  <th>COURSE</th>
+                  <th>FACULTY</th>
+                  <th>APPLIED</th>
+                  <th>SEATS</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr> 
+                  <td>Strategic Management</td>
+                  <td>A Dutta</td>
+                  <td>55</td>
+                  <td>1000</td>
+                </tr>
+                <tr>
+                  <td>Marketing Management</td>
+                  <td>B Chakaraborty</td>
+                  <td>35</td>
+                  <td>1000</td>
+                </tr>
+                <tr>
+                  <td>Indian Legal System</td>
+                  <td>B Chakaraborty</td>
+                  <td>35</td>
+                  <td>1000</td>
+                </tr>
+                <tr>
+                  <td>Valuation</td>
+                  <td>B Chakaraborty</td>
+                  <td>85</td>
+                  <td>1000</td>
+                </tr>
+                <tr>
+                  <td>Corporate Finance</td>
+                  <td>B Chakaraborty</td>
+                  <td>85</td>
+                  <td>1000</td>
+                </tr>
+                <tr>
+                  <td>Business Ethics</td>
+                  <td>R Mitter</td>
+                  <td>35</td>
+                  <td>1000</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="span4">
+            <h5>Course Select</h5>
+            <select>
+              <option>--------</option>
+              <option>Strategic Management</option>
+              <option>Business Ethics</option>
+              <option>Marketing Management</option>
+              <option>Corporate Finance</option>
+              <option>Valuation</option>
+              <option>Indian Legal System</option>
+            </select>
+          </div>
+        </div>
+      </section>
+
+
+      <section id="slot2">
+        <div class="page-header">
+          <h3>Slot 2</h3>
+        </div>
+        <div class="row">
+          <div class="span4">
+            <h5>Course Select</h5>
+            <select>
+              <option>--------</option>
+              <option>Strategic Management</option>
+              <option>Business Ethics</option>
+              <option>Marketing Management</option>
+              <option>Corporate Finance</option>
+              <option>Valuation</option>
+              <option>Indian Legal System</option>
+            </select>
+          </div>
+          <div class="span12">
+            <table class="bordered-table zebra-striped">
+              <thead>
+                <tr>
+                  <th>COURSE</th>
+                  <th>FACULTY</th>
+                  <th>APPLIED</th>
+                  <th>SEATS</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr> 
+                  <td>Strategic Management</td>
+                  <td>A Dutta</td>
+                  <td>55</td>
+                  <td>1000</td>
+                </tr>
+                <tr>
+                  <td>Marketing Management</td>
+                  <td>B Chakaraborty</td>
+                  <td>35</td>
+                  <td>1000</td>
+                </tr>
+                <tr>
+                  <td>Indian Legal System</td>
+                  <td>B Chakaraborty</td>
+                  <td>35</td>
+                  <td>1000</td>
+                </tr>
+                <tr>
+                  <td>Valuation</td>
+                  <td>B Chakaraborty</td>
+                  <td>85</td>
+                  <td>1000</td>
+                </tr>
+                <tr>
+                  <td>Corporate Finance</td>
+                  <td>B Chakaraborty</td>
+                  <td>85</td>
+                  <td>1000</td>
+                </tr>
+                <tr>
+                  <td>Business Ethics</td>
+                  <td>R Mitter</td>
+                  <td>35</td>
+                  <td>1000</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+
+
+      <section id="slot3">
+        <div class="page-header">
+          <h3>Slot 3</h3>
+        </div>
+        <div class="row">
+          <div class="span4">
+            <h5>Course Select</h5>
+            <select>
+              <option>--------</option>
+              <option>Strategic Management</option>
+              <option>Business Ethics</option>
+              <option>Marketing Management</option>
+              <option>Corporate Finance</option>
+              <option>Valuation</option>
+              <option>Indian Legal System</option>
+            </select>
+          </div>
+          <div class="span12">
+            <table class="bordered-table zebra-striped">
+              <thead>
+                <tr>
+                  <th>COURSE</th>
+                  <th>FACULTY</th>
+                  <th>APPLIED</th>
+                  <th>SEATS</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr> 
+                  <td>Strategic Management</td>
+                  <td>A Dutta</td>
+                  <td>55</td>
+                  <td>1000</td>
+                </tr>
+                <tr>
+                  <td>Marketing Management</td>
+                  <td>B Chakaraborty</td>
+                  <td>35</td>
+                  <td>1000</td>
+                </tr>
+                <tr>
+                  <td>Indian Legal System</td>
+                  <td>B Chakaraborty</td>
+                  <td>35</td>
+                  <td>1000</td>
+                </tr>
+                <tr>
+                  <td>Valuation</td>
+                  <td>B Chakaraborty</td>
+                  <td>85</td>
+                  <td>1000</td>
+                </tr>
+                <tr>
+                  <td>Corporate Finance</td>
+                  <td>B Chakaraborty</td>
+                  <td>85</td>
+                  <td>1000</td>
+                </tr>
+                <tr>
+                  <td>Business Ethics</td>
+                  <td>R Mitter</td>
+                  <td>35</td>
+                  <td>1000</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+
+      <div class="actions courseSubmit">
+        <input type="submit" class="btn primary" name="" value="Submit Courses" id=""/>
+        <a href="#top">Top</a>
+      </div>
+
+      <footer>
+        <p>&copy; IIM Calcutta - Internet Solutions Group 2012</p>
+      </footer>
+
+    </div> <!-- /container -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script src="assets/js/smooth-scroll.min.js" type="text/javascript" charset="utf-8"></script>
+    <script type="text/javascript" language="javascript" charset="utf-8">
+    //<![CDATA[
+    $('.nav li a').smoothScroll();
+    $('a.brand').smoothScroll({offset: -100});
+    $('.courseSubmit a').smoothScroll({offset: -100});
+    //]]>
+    </script>
+  </body>
+</html>
